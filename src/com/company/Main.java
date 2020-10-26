d1fde2da4943f683a6307b4c94352d97e276340f
cyberN0mad <s.baitur@mail.ru>
2020-10-26 22:27:40 +0600

    Obnovleno

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Runner runner1 = new Runner("Runner1");
+        Runner runner2 = new Runner("Runner2");
+        Runner runner3 = new Runner("Runner3");
+        Runner runner4 = new Runner("Runner4");
+        Runner runner5 = new Runner("Runner5");
 
-        new Runner("Runner").start();
+
+        runner1.setRunnerFS(runner5, runner2);
+        runner2.setRunnerFS(runner1, runner3);
+        runner3.setRunnerFS(runner2, runner4);
+        runner4.setRunnerFS(runner3, runner5);
+        runner5.setRunnerFS(runner4, runner1);
+
+
+        runner1.start();
+
+
+
     }
 }
 
-//  Изначально писал через try и catch
-//        try {
-//        sleep(500);
-//        } catch (Exception ignored) {
-//        }
-//        System.out.println(getName() + " бежит к " + runnerF.getName());
-//        try {
-//        sleep(500);
-//  Но что то пошло не так...
+//    public static void main(String[] args) {
+//
+//        new Runner("Runner").start();
+//    }
+//}
+
+
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,40 +1,108 @@
 package com.company;
 
 public class Runner extends Thread {
+    private Runner runnerF;
+    private Runner runnerS;
+
     public Runner(String name) {
         super(name);
     }
 
-    @Override
-    public void run() {
+    public void setRunnerFS(Runner runnerF, Runner runnerS) {
+        this.runnerF = runnerF;
+        this.runnerS = runnerS;
 
-        for (int i = 1, b = 2; i < 6; i++, b++) {
-            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
-            if (b == 6) {
-                System.out.printf("%s бежит к финишу \n", Thread.currentThread().getName() + " " + i);
-                System.out.printf("%s бежит к Runner 4 \n", Thread.currentThread().getName() + " " + i);
-            } else {
-                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, b);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-
-                }
-            }
-        }
-        for (int i = 4, j = 4; i >= 1; i--, j--) {
-            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
-            if (i == 1) {
-                interrupt();
-            }
-            else {
-                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-
-                }
-            }
-        }
     }
+
+    @Override
+    public synchronized void run() {
+
+        if (getName().equals("Runner5")) {
+            System.out.println("_________________________");
+            System.out.println(getName() + " берет палочку ");
+            System.out.println(getName() + " бежит к финишу ");
+            System.out.println("_________________________");
+            try {
+                sleep(500);
+            } catch (Exception ignored) {
+            }
+            System.out.println(getName() + " бежит к " + runnerF.getName());
+            try {
+                sleep(500);
+            } catch (Exception ignored) {
+            }
+        } else {
+            System.out.println("___________________");
+            System.out.println(getName() + " берет палочку ");
+            System.out.println(getName() + " бежит к " + runnerS.getName());
+            try {
+                sleep(500);
+
+            }catch (Exception ignored) {
+            }
+
+            runnerS.start();
+            try {
+                runnerS.join();
+            }catch (Exception ignored) {
+            }
+            System.out.println(getName() + " берет палочку ");
+            System.out.println("___________________");
+            try {
+                sleep(500);
+                if (!getName().equals("Runner5") && !getName().equals("Runner1")) {
+                    System.out.println(getName() + " бежит к " + runnerF.getName());
+                    try {
+                        sleep(500);
+                    }catch (Exception ignored) {
+                    }
+                }
+            }catch (Exception ignored){
+
+            }
+        }
+
+
+    }
+
+
 }
+//    public Runner(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//        for (int i = 1, b = 2; i < 6; i++, b++) {
+//            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
+//            if (b == 6) {
+//                System.out.printf("%s бежит к финишу \n", Thread.currentThread().getName() + " " + i);
+//                System.out.printf("%s бежит к Runner 4 \n", Thread.currentThread().getName() + " " + i);
+//            } else {
+//                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, b);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//
+//                }
+//            }
+//        }
+//        for (int i = 4, j = 4; i >= 1; i--, j--) {
+//            System.out.printf("%s берет палочку \n", Thread.currentThread().getName() + " " + i);
+//            if (i == 1) {
+//                interrupt();
+//            }
+//            else {
+//                System.out.printf("%s бежит к Runner %d \n", Thread.currentThread().getName() + " " + i, j);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//
+//                }
+//            }
+//        }
+//    }
+//}
+
+
